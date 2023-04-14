014a6e98a54768382f49f36668c98ac5f34d5356
NAKAJIMA Yusaku <yupong7@gmail.com>
2023-04-14 22:50:51 +0900

    Intro component component-overview

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ComponentOverviewComponent } from './component-overview.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ComponentOverviewComponent,
   ],
   imports: [
     BrowserModule
---
+++ src/app/component-overview.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'app-component-overview',
+  templateUrl: './component-overview.component.html',
+  styleUrls: ['./component-overview.component.css']
+})
+export class ComponentOverviewComponent {
+    
+}
